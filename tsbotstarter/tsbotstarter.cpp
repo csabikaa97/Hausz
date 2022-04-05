b81f99baff436ca278ea813b7d07905dcd305452
csabikaa97 <csabikaa97@gmail.com>
2022-04-05 13:50:33 +0000

    dsa

--- tsbotstarter/tsbotstarter.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-//	system("touch teszt");
-	system("sh ./tsbotstarter.sh");
-	return 0;
-}
--- tsbotstarter/tsbotstarter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/csabikaa97/ts3audiobot
-
-rm mukszik
-touch mukszik
-
-dotnet-runtime-22.dotnet TS3AudioBot.dll > log.txt
