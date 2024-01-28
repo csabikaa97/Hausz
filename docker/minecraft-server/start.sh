1546d11568984e18b8eaed6f3406763872c7ce20
csabikaa97 <csabikaa97@gmail.com>
2024-01-28 23:47:36 +0100

    Minecraft szerver memória használat módosítás

--- docker/minecraft-server/start.sh
+++ docker/minecraft-server/start.sh
@@ -2,4 +2,4 @@ cp /minecraft_build_files/purpur.jar /minecraft/purpur.jar
 cp /minecraft_build_files/eula.txt /minecraft/eula.txt
 cp -r /minecraft_build_files/plugins /minecraft/plugins
 cd /minecraft
-exec /jdk/jdk-21.0.1+12/bin/java -XX:+UseLargePages -XX:LargePageSizeInBytes=2M -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -XX:ShenandoahGCMode=iu -XX:+UseNUMA -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -Dfile.encoding=UTF-8 -server -Xms1G -Xmx1G -jar purpur.jar --nogui
+exec /jdk/jdk-21.0.1+12/bin/java -XX:+UseLargePages -XX:LargePageSizeInBytes=2M -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -XX:ShenandoahGCMode=iu -XX:+UseNUMA -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -Dfile.encoding=UTF-8 -server -Xms8G -Xmx8G -jar purpur.jar --nogui
